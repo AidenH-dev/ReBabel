3f769b0a65f339841871efb8bd5180355bf7b37b
Aiden Habboub <aiden.habboub@gmail.com>
2024-11-09 22:00:13 -0500

    additional syling

--- src/pages/learn/index.js
+++ src/pages/learn/index.js
@@ -1,12 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { FaArrowLeftLong, FaArrowRight, FaArrowRightLong } from "react-icons/fa6";
+import {
+  FaArrowLeftLong,
+  FaArrowRight,
+  FaArrowRightLong,
+} from "react-icons/fa6";
 import { CiPlay1 } from "react-icons/ci";
 import { TbDivide, TbLoader3 } from "react-icons/tb";
 import { useState } from "react";
 import { useEffect } from "react";
-import Chart from 'chart.js/auto';
+import Chart from "chart.js/auto";
 import { useRef } from "react";
 
 export default function Learn() {
@@ -67,14 +71,30 @@ export default function Learn() {
   useEffect(() => {
     if (responseMessageGrade) {
       const chartsData = {
-        grammarChart: { value: responseMessageGrade.grades.grammar_and_structure, max: 20 },
-        vocabularyChart: { value: responseMessageGrade.grades.vocabulary_and_expression, max: 20 },
-        spellingChart: { value: responseMessageGrade.grades.spelling_and_script_accuracy, max: 20 },
-        politenessChart: { value: responseMessageGrade.grades.politeness_and_cultural_appropriateness, max: 20 },
-        fluencyChart: { value: responseMessageGrade.grades.fluency_and_naturalness, max: 20 },
+        grammarChart: {
+          value: responseMessageGrade.grades.grammar_and_structure,
+          max: 20,
+        },
+        vocabularyChart: {
+          value: responseMessageGrade.grades.vocabulary_and_expression,
+          max: 20,
+        },
+        spellingChart: {
+          value: responseMessageGrade.grades.spelling_and_script_accuracy,
+          max: 20,
+        },
+        politenessChart: {
+          value:
+            responseMessageGrade.grades.politeness_and_cultural_appropriateness,
+          max: 20,
+        },
+        fluencyChart: {
+          value: responseMessageGrade.grades.fluency_and_naturalness,
+          max: 20,
+        },
       };
 
-      Object.keys(chartsData).forEach(chartId => {
+      Object.keys(chartsData).forEach((chartId) => {
         const { value, max } = chartsData[chartId];
 
         // Destroy existing chart if it exists
@@ -83,20 +103,28 @@ export default function Learn() {
         }
 
         // Create new chart
-        chartRefs.current[chartId] = new Chart(document.getElementById(chartId), {
-          type: 'doughnut',
-          data: {
-            datasets: [{
-              data: [value, max - value],
-              backgroundColor: ['#1c64f2', '#e5e7eb'],
-              borderWidth: 0,
-            }],
-          },
-          options: {
-            cutout: '80%',
-            plugins: { tooltip: { enabled: false }, legend: { display: false } },
+        chartRefs.current[chartId] = new Chart(
+          document.getElementById(chartId),
+          {
+            type: "doughnut",
+            data: {
+              datasets: [
+                {
+                  data: [value, max - value],
+                  backgroundColor: ["#1c64f2", "#e5e7eb"],
+                  borderWidth: 0,
+                },
+              ],
+            },
+            options: {
+              cutout: "80%",
+              plugins: {
+                tooltip: { enabled: false },
+                legend: { display: false },
+              },
+            },
           }
-        });
+        );
       });
     }
   }, [responseMessageGrade]);
@@ -105,7 +133,10 @@ export default function Learn() {
     <main className="flex flex-col items-center justify-between h-screen overflow-hidden px-10 py-4 relative">
       {/* Floating back button at the top left */}
       <div className="absolute top-0 left-0 m-4">
-        <Link href="/" className="text-left text-4xl font-semibold leading-tight">
+        <Link
+          href="/"
+          className="text-left text-4xl font-semibold leading-tight"
+        >
           <FaArrowLeftLong />
         </Link>
       </div>
@@ -117,7 +148,6 @@ export default function Learn() {
         </Head>
 
         <main className="flex flex-row mt-50">
-
           <div className="flex flex-row items-center justify-center flex-wrap w-screen max-w-screen-md mt-2 mx-auto">
             <div className="grid grid-flow-col m-4 max-w-screen-md basis-2/5 p-6 text-left no-underline border border-gray-200 rounded-lg transition-colors duration-150 ">
               <div>
@@ -126,24 +156,35 @@ export default function Learn() {
                     Generate A Sentance <span className="ml-2"></span>
                   </h3>
                   <button
-                    className={`p-2 bg-blue-600 text-white rounded flex items-center justify-center  ${isLoading ? "disabled opacity-50 cursor-not-allowed" : ""
-                      }`} // Add dynamic button classes
+                    className={`p-2 bg-blue-600 text-white rounded flex items-center justify-center  ${
+                      isLoading ? "disabled opacity-50 cursor-not-allowed" : ""
+                    }`} // Add dynamic button classes
                     onClick={handleButtonClick}
                     disabled={isLoading} // Disable button using isLoading state
                   >
-                    {isLoading ? <TbLoader3 className="animate-spin" /> : <CiPlay1 />}
+                    {isLoading ? (
+                      <TbLoader3 className="animate-spin" />
+                    ) : (
+                      <CiPlay1 />
+                    )}
                   </button>
                 </div>
                 <div className="flex items-center my-10">
                   {responseMessage && ( // Conditionally render response message
-                    <p className="font-semibold mx-2 text-green-500">{responseMessage}</p>
+                    <p className="font-semibold mx-2 text-green-500">
+                      {responseMessage}
+                    </p>
                   )}
                 </div>
                 <h3 className="m-0 mb-4 font-semibold text-2xl flex items-center">
                   Check <span className="ml-2"></span>
                 </h3>
                 <div className="flex items-center">
-                  <input type="text" className="flex-grow p-2 border border-gray-300 mr-2 rounded" placeholder="Type your message..." />
+                  <input
+                    type="text"
+                    className="flex-grow p-2 border border-gray-300 mr-2 rounded"
+                    placeholder="Type your message..."
+                  />
                   <button
                     className="p-2 bg-blue-600 text-white rounded flex items-center justify-center"
                     onClick={handleButtonClickGrade}
@@ -156,7 +197,6 @@ export default function Learn() {
               <div className="mx-16">
                 {responseMessageGrade && (
                   <div className="ml-8 max-w-full w-full bg-white rounded-lg shadow-lg p-6 mt-2">
-
                     <div className="flex items-center mb-2">
                       <div className="relative w-16 h-16">
                         <canvas id="grammarChart"></canvas>
@@ -164,66 +204,102 @@ export default function Learn() {
                           {responseMessageGrade.grades.grammar_and_structure}/20
                         </span>
                       </div>
-                      <span className="ml-2 text-lg text-gray-700">Grammar and Structure</span>
+                      <span className="flex items-center ml-2">
+                        <span className="px-4 py-2 text-blue-700 bg-blue-100 text-lg font-semibold rounded-lg">
+                          Grammar and Structure
+                        </span>
+                      </span>
                     </div>
 
                     <div className="flex items-center mb-2">
                       <div className="relative w-16 h-16">
                         <canvas id="vocabularyChart"></canvas>
                         <span className="absolute inset-0 flex items-center justify-center text-sm font-bold text-gray-900">
-                          {responseMessageGrade.grades.vocabulary_and_expression}/20
+                          {
+                            responseMessageGrade.grades
+                              .vocabulary_and_expression
+                          }
+                          /20
                         </span>
                       </div>
-                      <span className="ml-2 text-lg text-gray-700">Vocabulary and Expression</span>
+                      <span className="flex items-center ml-2">
+                        <span className="px-4 py-2 text-green-700 bg-green-100 text-lg font-semibold rounded-lg">
+                          Vocabulary and Expression
+                        </span>
+                      </span>
                     </div>
 
                     <div className="flex items-center mb-2">
                       <div className="relative w-16 h-16">
                         <canvas id="spellingChart"></canvas>
                         <span className="absolute inset-0 flex items-center justify-center text-sm font-bold text-gray-900">
-                          {responseMessageGrade.grades.spelling_and_script_accuracy}/20
+                          {
+                            responseMessageGrade.grades
+                              .spelling_and_script_accuracy
+                          }
+                          /20
                         </span>
                       </div>
-                      <span className="ml-2 text-lg text-gray-700">Spelling and Script Accuracy</span>
+                      <span className="flex items-center ml-2">
+                        <span className="px-4 py-2 text-purple-700 bg-purple-100 text-lg font-semibold rounded-lg">
+                          Spelling and Script Accuracy
+                        </span>
+                      </span>
                     </div>
 
                     <div className="flex items-center mb-2">
                       <div className="relative w-16 h-16">
                         <canvas id="politenessChart"></canvas>
                         <span className="absolute inset-0 flex items-center justify-center text-sm font-bold text-gray-900">
-                          {responseMessageGrade.grades.politeness_and_cultural_appropriateness}/20
+                          {
+                            responseMessageGrade.grades
+                              .politeness_and_cultural_appropriateness
+                          }
+                          /20
                         </span>
                       </div>
-                      <span className="ml-2 text-lg text-gray-700">Politeness and Cultural Appropriateness</span>
+                      <span className="flex items-center ml-2">
+                        <span className="px-4 py-2 text-orange-700 bg-orange-100 text-lg font-semibold rounded-lg">
+                          Politeness and Cultural Accuracy
+                        </span>
+                      </span>
                     </div>
 
                     <div className="flex items-center mb-2">
                       <div className="relative w-16 h-16">
                         <canvas id="fluencyChart"></canvas>
                         <span className="absolute inset-0 flex items-center justify-center text-sm font-bold text-gray-900">
-                          {responseMessageGrade.grades.fluency_and_naturalness}/20
+                          {responseMessageGrade.grades.fluency_and_naturalness}
+                          /20
                         </span>
                       </div>
-                      <span className="ml-2 text-lg text-gray-700">Fluency and Naturalness</span>
+                      <span className="flex items-center ml-2">
+                        <span className="px-4 py-2 text-red-700 bg-red-100 text-lg font-semibold rounded-lg">
+                          Fluency and Naturalness
+                        </span>
+                      </span>
                     </div>
 
                     {/* Total Grade Section */}
                     <div className="flex justify-between items-center mt-6 pt-4 border-t border-gray-200">
-                      <span className="text-xl font-bold text-gray-900">Total Grade</span>
                       <span className="text-xl font-bold text-gray-900">
-                        {Object.values(responseMessageGrade.grades).reduce((sum, grade) => sum + grade, 0)}/100
+                        Total Grade
+                      </span>
+                      <span className="text-xl font-bold text-gray-900">
+                        {Object.values(responseMessageGrade.grades).reduce(
+                          (sum, grade) => sum + grade,
+                          0
+                        )}
+                        /100
                       </span>
                     </div>
                   </div>
                 )}
-              </div>        
-
+              </div>
             </div>
-                      
           </div>
         </main>
       </div>
     </main>
-
   );
 }
