7e4e6be231ffd2baabf9843cdfe0bf34258f0457
aiden habboub <aiden.habboub@gmail.com>
2025-01-29 17:45:57 -0500

    updated styling

--- src/components/report-issue.js
+++ src/components/report-issue.js
@@ -1,42 +1,57 @@
 import React from 'react';
 import { TbMessageReport } from 'react-icons/tb';
 
-function ReportIssueButton({ onClick }) {
-    const handleReportClick = () => {
-        // Opens the Google Form in a new browser tab
-        window.open('https://forms.gle/avVR1fyjsFFsfoBQA', '_blank');
-      };
-    
-      return (
-        <button
-          onClick={handleReportClick}
-          className="
-            fixed 
-            bottom-6 
-            right-6 
-            z-50 
-            flex 
-            items-center 
-            gap-2 
-            rounded-full 
-            bg-red-600 
-            px-4 
-            py-3 
-            text-white 
-            shadow-lg 
-            transition-colors 
-            duration-200 
-            hover:bg-red-700 
-            active:outline-none 
-            active:ring-2 
-            active:ring-red-600 
-            active:ring-offset-2
-          "
-        >
-          <TbMessageReport className="text-xl" />
-          <span className="hidden sm:inline">Report Issue</span>
-        </button>
-      );
+function ReportIssueButton() {
+  const handleReportClick = () => {
+    // Opens the Google Form in a new browser tab
+    window.open('https://forms.gle/avVR1fyjsFFsfoBQA', '_blank');
+  };
+
+  return (
+    <button
+      onClick={handleReportClick}
+      // The 'group' class is important so child elements can detect hover via `group-hover:`
+      className="
+        group
+        fixed 
+        bottom-6 
+        right-6 
+        z-50 
+        flex 
+        items-center 
+        rounded-full 
+        bg-red-600 
+        px-5 
+        py-3
+        text-white 
+        shadow-lg 
+        transition-colors
+        duration-200 
+        hover:bg-red-500 
+        active:outline-none 
+        active:ring-2 
+        active:ring-red-600 
+        active:ring-offset-2
+      "
+    >
+      {/* Increased icon size to text-2xl */}
+      <TbMessageReport className="text-2xl" />
+      <span
+        className="
+          ml-0
+          max-w-0
+          overflow-hidden
+          whitespace-nowrap
+          transition-all
+          duration-200
+          group-hover:ml-2
+          group-hover:max-w-xs
+        "
+      >
+        Report Issue
+      </span>
+    </button>
+  );
 }
 
 export default ReportIssueButton;
