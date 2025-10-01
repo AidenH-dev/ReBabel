0587d9d635cd69259fd08695bc6eb507fa994ea3
aiden habboub <aiden.habboub@gmail.com>
2025-10-01 12:31:10 -0400

    fix: env variable misconfig

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,8 +36,8 @@ function PostHogAuthBridge() {
 export default function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    posthog.init(process.env.NEXT_POSTHOG_KEY, {
-      api_host: process.env.NEXT_POSTHOG_HOST || 'https://us.i.posthog.com',
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       person_profiles: 'identified_only',
       defaults: '2025-05-24',
       session_recording: {
